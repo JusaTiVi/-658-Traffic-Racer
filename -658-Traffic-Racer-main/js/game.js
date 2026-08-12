c318a5c7b9ad86b179a3e1c56b9e97cd0da34871
Yevhen <progro12321@gmail.com>
2026-08-12 13:28:19 +0300

    initial implementation of the driving prototype
    Created the road layout and boundaries
    Added player vehicle asset
    Implemented movement constraints (car stays within road area)
    Added infinite background scrolling effect
    Integrated responsive player controls (steering / acceleration)

---
+++ -658-Traffic-Racer-main/js/game.js
@@ -0,0 +1,127 @@
+const canvas = document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
+
+
+const bgImg = new Image();
+bgImg.src = "../pictures/bg.png"; 
+
+const carImg = new Image();
+carImg.src = "../pictures/car.png";
+
+
+
+//hitbox in background
+
+const Bghitbox = {
+    xLeft : 120,
+    xRight: 590, 
+    yUp:230, 
+    yDown: 950,
+}
+
+
+
+//spawn point 
+
+const player = {
+    x: 350,         
+    y: 230,         
+    width: 100,
+    height: 180
+};
+// speed with wich go BG 
+let BgSpeed = 5;
+// speed with wich go car 
+let speed = 10;
+// vertical position of the image on the screen
+let bgY = 0; 
+
+
+// creates an infinite background scroll (loop) using two images that alternate with each other
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    bgY += BgSpeed;
+
+    if (bgY >= canvas.height) {
+        bgY = 0;  //123
+    }
+
+    ctx.drawImage(bgImg, 0, bgY, canvas.width, canvas.height);
+
+    ctx.drawImage(bgImg, 0, bgY - canvas.height, canvas.width, canvas.height);
+
+
+    ctx.drawImage(carImg, player.x, player.y, player.width, player.height);
+    
+}
+// for debugging, so that things load one by one
+
+let imagesLoaded = 0;
+function imageLoadedCallback() {
+    imagesLoaded++;
+    if (imagesLoaded === 2) {
+        draw(); 
+    }
+}
+
+// control 
+window.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft" && player.x > Bghitbox.xLeft) {
+        player.x -= speed;
+        draw();
+    }
+
+    if (event.key === "ArrowRight" && player.x < Bghitbox.xRight) { 
+        player.x += speed;
+
+        draw();
+    }
+
+    if (event.key === "ArrowUp" && player.y > Bghitbox.yUp) {
+        player.y -= speed;
+        draw();
+    }
+
+    if (event.key === "ArrowDown" && player.y < Bghitbox.yDown) {
+        player.y += speed; 
+        draw();
+        
+    }
+    
+   
+    window.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowDown") {
+        BgSpeed = 3; // slows down if keydown presed 
+    }
+    });
+
+    window.addEventListener("keyup", function(event) {
+        if (event.key === "ArrowDown") {
+            BgSpeed = 5; // cancels
+        }
+    });
+
+
+});
+
+// the main loop that drives the game
+
+function gameCycle() {
+
+    draw(); 
+
+    requestAnimationFrame(gameCycle);
+}
+
+
+gameCycle();
+
+bgImg.onload = imageLoadedCallback;
+carImg.onload = imageLoadedCallback;
+
+// I slightly stretched the image to suit different devices. XD 
+// Make it so that obstacles appear once the first background has changed.
+// I marked the moment with the comment "123".
+// Although, you'd better just do as you see fit.
